b10e74b5e78fa29ab2a82cb874ebb78caf937d92
tekater <120014627+tekater@users.noreply.github.com>
2023-01-13 21:05:33 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setFirstName("t");
+        user.setLastName("");
+        System.out.println(user.getFullName());
+    }
+}
